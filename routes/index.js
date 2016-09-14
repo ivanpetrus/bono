0f0213f842fd70e7620e1ca6c13446c1a2a6ece9
ivanpetrus <ivan_petrus@hotmail.com>
2016-09-14 22:51:13 +0300

    no message

--- routes/index.js
+++ routes/index.js
@@ -66,6 +66,7 @@ module.exports = function (app) {
                                     }
                                 });
                             }
+                            else {console.error("reminder object is null")}
                         });
                     }
                     catch (err){
