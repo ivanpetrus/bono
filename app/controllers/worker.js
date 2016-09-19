6247dde1c15176e088ef2dbd2a733bfaeb0ff737
ivanpetrus <ivan_petrus@hotmail.com>
2016-09-19 15:28:27 +0300

    no message

--- app/controllers/worker.js
+++ app/controllers/worker.js
@@ -6,109 +6,100 @@ var ds = require('../../data/ds');
 var slacko = require('./bonoo');
 
 var do_work = function (task) {
-   // var tasks = [];
+    // var tasks = [];
 
     //process.on('message', function (message) {
-      //  tasks.push(message);
+    //  tasks.push(message);
     //});
     //setInterval(function () {
-      //  if (tasks.length == 0) {
-        //    return;
-        //}
-        //var task = tasks.pop();
-        if (task.type == "reminder") {
-            var team = task.data;
-            var ts = new Date();
-            var time = parseInt(ts.getHours().toString() + ts.getMinutes().toString());
-            var gt = time - 2;
-            var lt = time + 2;
-            var rtm = task.rtm;
-            ds.get_reminders(team.id, {status: "none", time: {$gt: gt, $lt: lt}}, function (err, array) {
-                if (array != null) {
-                    for (c in array) {
-                        var item = array[c];
-                        rtm.send_reminder_message("Hey " + item.name + "! could you please report your hours " +
+    //  if (tasks.length == 0) {
+    //    return;
+    //}
+    //var task = tasks.pop();
+    if (task.type == "reminder") {
+        var team = task.data;
+        var ts = new Date();
+        var time = parseInt(ts.getHours().toString() + ts.getMinutes().toString());
+        var gt = time - 2;
+        var lt = time + 2;
+
+        ds.get_reminders(team.id, {status: "none", time: {$gt: gt, $lt: lt}}, function (err, array) {
+            if (array != null) {
+                for (c in array) {
+                    var item = array[c];
+                    slacko.connect(team, function () {
+                        slacko.send_reminder_message("Hey " + item.name + "! could you please report your hours " +
                             "into time reporting tools. after it just simply send me message yes or no", team.id, item.channel);
                         item.status = "sent";
                         item.save();
-                    }
-
+                    });
                 }
-            })
-            ds.get_reminders(team.id, {time: {$gt: time}}, function (err, array) {
-                if (array != null) {
-                    for (c in array) {
-                        var item = array[c];
-                        item.status = "none";
-                        item.save();
-                    }
 
+            }
+        })
+        ds.get_reminders(team.id, {time: {$gt: time}}, function (err, array) {
+            if (array != null) {
+                for (c in array) {
+                    var item = array[c];
+                    item.status = "none";
+                    item.save();
                 }
-            })
+
+            }
+        })
 
 
-        }
-   // }, 1000);
+    }
+    // }, 1000);
 }
 
 var do_master_work = function () {
-    console.log('worker');
+    //  console.log('worker');
     ds.get_all_teams(function (err, array) {
         if (err == null && array != null) {
             for (var t in array) {
                 var team = array[t];
-                slacko.connect(team, function (rtm) {
-                   /* worker.send({
-                        type: "reminder",
-                        from: "master",
-                        data: team,
-                        rtm: rtm
-                    });
-                    */
-                   do_work({
-                       type: "reminder",
-                       from: "master",
-                       data: team,
-                       rtm: rtm
-                   });
+                do_work({
+                    type: "reminder",
+                    from: "master",
+                    data: team
                 });
             }
         }
     })
 }
 
-module.exports = {
-    run: function (action) {
+exports.run = function () {
 
-       // if (cluster.isMaster) {
-            action();
-        /*
-            var wids = [];
-            var cwid = 0;
+    // if (cluster.isMaster) {
 
-            var workers = 1//require('os').cpus().length;
+    /*
+     var wids = [];
+     var cwid = 0;
 
-            for (var i = 0; i < workers; i++) {
-                cluster.fork();
-            }
-            for (var wid in cluster.workers) {
-                wids.push(wid);
-            }
-            */
-            setInterval(function () {
-                do_master_work();
-               // var worker = cluster.workers[wids[cwid]];
-                //if (worker) {
-                   // do_master_work(worker);
-                //}
-                //cwid++;
-                //if (cwid >= wids.length) {
-                  //  cwid = 0;
-               // }
-            }, 1000);
+     var workers = 1//require('os').cpus().length;
+
+     for (var i = 0; i < workers; i++) {
+     cluster.fork();
+     }
+     for (var wid in cluster.workers) {
+     wids.push(wid);
+     }
+     */
+    setInterval(function () {
+        do_master_work();
+        // var worker = cluster.workers[wids[cwid]];
+        //if (worker) {
+        // do_master_work(worker);
         //}
-        //else {
-          //  do_work();
-        //}
-    }
+        //cwid++;
+        //if (cwid >= wids.length) {
+        //  cwid = 0;
+        // }
+    }, 1000);
+    //}
+    //else {
+    //  do_work();
+    //}
 }
+exports.run();
